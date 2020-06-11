07e2540d51066f92f380ce2247a568c5c15ff3c9
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-11 20:59:54 -0300

    feat: Add middleware

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 projectData = {};
 // Require Express to run server and routes
 const express = require('express');
+const bodyParser = require('body-parser');
 // Start up an instance of app
 const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 
