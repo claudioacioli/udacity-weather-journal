e48a719fcb5ea472fa4e6f2f87ab5bff902efe8d
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-11 22:34:51 -0300

    feat: Add api info

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,13 @@
-/* Global Variables */
+document.addEventListener("DOMContentLoaded", e => {
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+  /* Global Variables */
+  const 
+    OPEN_WEATHER_API_URL = 'api.openweathermap.org',
+    OPEN_WEATHER_API_KEY = 'aa8234745b091ee3bb02c21e583fcc34'
+  ;
+  // Cors for cross origin allowance
+  // Create a new date instance dynamically with JS
+  let d = new Date();
+  let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+});
