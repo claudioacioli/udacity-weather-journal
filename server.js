8073c6d1825e39c8756fad5fc29a883d773a88fb
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-11 22:35:59 -0300

    refactor: Remove api data

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 // Setup empty JS object to act as endpoint for all routes
 const projectData = {};
-const OPEN_WEATHER_API_URL = 'api.openweathermap.org';
-const OPEN_WEATHER_API_KEY = 'aa8234745b091ee3bb02c21e583fcc34';
 // Instance of app
 const app = express();
 // Middleware
