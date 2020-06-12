dc4eb695a0eefca86b6289cc38f7f3e2ccc6e0ab
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-11 21:30:30 -0300

    refactor: Restructure code

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-const OPEN_WEATHER_API_URL = 'api.openweathermap.org';
-const OPEN_WEATHER_API_KEY = 'aa8234745b091ee3bb02c21e583fcc34';
 // Require Express to run server and routes
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// Start up an instance of app
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {};
+const OPEN_WEATHER_API_URL = 'api.openweathermap.org';
+const OPEN_WEATHER_API_KEY = 'aa8234745b091ee3bb02c21e583fcc34';
+// Instance of app
 const app = express();
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
 app.use(cors());
-// Initialize the main project folder
+// Static content
 app.use(express.static('website'));
-
 // Setup Server
-const port = 8080;
+const PORT = 8080;
 const listining = () => {
   console.log('Server On');
-  console.log(`Listening on http://localhost:${port}`);
+  console.log(`Listening on http://localhost:${PORT}`);
 };
 app.listen(port, listining);
 
