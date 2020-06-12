373572ffd062c34e1ff37717bb425f6196df8467
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-12 00:06:28 -0300

    feat: Add routes

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 // Setup empty JS object to act as endpoint for all routes
-const projectData = {};
+const projectData = [];
+const readData = (req, res) => {
+  res.send(projectData);
+};
+const createData = (req, res) => {
+  projectData.push(JSON.parse(res.body));
+  res.send();
+};
 // Instance of app
 const app = express();
 // Middleware
@@ -12,6 +19,9 @@ app.use(bodyParser.json());
 app.use(cors());
 // Static content
 app.use(express.static('public'));
+// Routes
+app.get("/data", readData);
+app.post("/data", createData);
 // Setup Server
 const PORT = 8080;
 const listining = () => {
