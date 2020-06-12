62339d19a09939c1934e48128856b66df2ea3e25
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-12 00:33:06 -0300

    feat: Map DOM elements

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,15 @@ document.addEventListener("DOMContentLoaded", e => {
   /* Global Variables */
   const 
     OPEN_WEATHER_API_URL = 'https://api.openweathermap.org',
-    OPEN_WEATHER_API_KEY = 'aa8234745b091ee3bb02c21e583fcc34'
+    OPEN_WEATHER_API_KEY = 'aa8234745b091ee3bb02c21e583fcc34',
+  /* DOM Elements */
+    zipElement = document.getElementById("zip"),
+    feelingsElement = document.getElementById("feelings"),
+    buttonElement = document.getElementById("generate"),
+    holderElement = document.getElementById("entryHolder"),
+    dateElement = document.getElementById("date"),
+    tempElement = document.getElementById("temp"),
+    contentElement = document.getElementById("content")
   ;
   // Cors for cross origin allowance
   // Create a new date instance dynamically with JS
@@ -45,8 +53,11 @@ document.addEventListener("DOMContentLoaded", e => {
       const result = await  getData(`${OPEN_WEATHER_API_URL}/data/2.5/weather?q=${city},${country}&APPID=${OPEN_WEATHER_API_KEY}`);
       console.log(result);
     }
+
   ;
 
   getWeatherDataFromCity("Brasilia", "BR");
+  getData("/data")
+    .then(result => console.log(result));
 
 });
