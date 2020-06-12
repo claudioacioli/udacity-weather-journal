ae9d79fbc3539b7f7a5a823c2402b038b8cf17fb
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-11 23:52:27 -0300

    feat: Add request utilities

--- public/js/app.js
+++ public/js/app.js
@@ -10,4 +10,43 @@ document.addEventListener("DOMContentLoaded", e => {
   let d = new Date();
   let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
 
+  const 
+    
+    getData = async url => {
+      const response = await fetch(url);
+      
+      try {
+        const result = await response.json();
+        return result;
+      } catch (ex) {
+        console.error("Error", ex);
+      }
+    },
+
+    postData = async (url, data={}) => {
+      const response = await fetch(url, {
+        method: "POST",
+        credentials: "same-origin",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      try {
+        const result = await response.json();
+        return result;
+      } catch (ex) {
+        console.error("Error", ex);
+      }
+    },
+    
+    getWeatherDataFromCity = async (city, country) => {
+      const result = await  getData(`${OPEN_WEATHER_API_URL}/data/2.5/weather?q=${city},${country}&APPID=${OPEN_WEATHER_API_KEY}`);
+      console.log(result);
+    }
+  ;
+
+  getWeatherDataFromCity("Brasilia", "BR");
+
 });
