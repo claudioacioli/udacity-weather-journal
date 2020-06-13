43747f4bfc86a900876461280a1bbf52aa14c1b4
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-13 12:30:07 -0300

    refactor: Extract api for another file

---
+++ public/js/api.js
@@ -0,0 +1,33 @@
+
+const  
+  
+  getData = async (url, mode="cors") => {
+    const response = await fetch(url, {method:"GET", mode});
+    
+    try {
+      const result = await response.json();
+      return result;
+    } catch (ex) {
+      console.error("Error", ex);
+    }
+  },
+
+  postData = async (url, data={}, mode="cors") => {
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      body: JSON.stringify(data),
+      mode,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    try {
+      const result = await response.json();
+      return result;
+    } catch (ex) {
+      console.error("Error", ex);
+    }
+  }
+;
--- public/js/app.js
+++ public/js/app.js
@@ -12,40 +12,7 @@ document.addEventListener("DOMContentLoaded", e => {
     holderElement = byId("entryHolder"),
     dateElement = byId("date"),
     tempElement = byId("temp"),
-    contentElement = byId("content")
-  ;
-
-  const 
-    
-    getData = async (url, mode="cors") => {
-      const response = await fetch(url, {method:"GET", mode});
-      
-      try {
-        const result = await response.json();
-        return result;
-      } catch (ex) {
-        console.error("Error", ex);
-      }
-    },
-
-    postData = async (url, data={}, mode="cors") => {
-      const response = await fetch(url, {
-        method: "POST",
-        credentials: "same-origin",
-        body: JSON.stringify(data),
-        mode,
-        headers: {
-          "Content-Type": "application/json"
-        }
-      });
-
-      try {
-        const result = await response.json();
-        return result;
-      } catch (ex) {
-        console.error("Error", ex);
-      }
-    },
+    contentElement = byId("content"),
 
     buildOpenWeatherUrl = (zip, country="BR") => {
       return `${OPEN_WEATHER_API_URL}?q=${zip},${country}&APPID=${OPEN_WEATHER_API_KEY}`;
