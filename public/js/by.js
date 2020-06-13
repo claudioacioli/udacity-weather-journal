ee04a95063b90edc44b212358e890d7cf29968a9
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-13 12:18:18 -0300

    feat: Create wrapper

---
+++ public/js/by.js
@@ -0,0 +1,39 @@
+/**
+ * Wrapper to acces the DOM elements
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * @Author: Claudio Acioli
+*/
+
+/**
+ * Start Wrapper Functions
+ *
+*/
+const 
+  
+  byId = (id, parent=document) =>
+    parent.getElementById(id)
+  ,
+
+  bySelector = (selector, parent=document) =>
+    parent.querySelector(selector)
+  ,
+
+  byAll = (selector, parent=document) => 
+    parent.querySelectorAll(selector)
+  ,
+
+  byName = (name, parent=document) =>
+    parent.getElementsByName(name)
+  ,
+
+  byClassName = (name, parent=document) =>
+    parent.getElementsByClassName(name)
+  ,
+
+  byTag = (tag, parent=document) =>
+    parent.getElementsByTagName(tag)
+;
